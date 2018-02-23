982c15d6332fa169766a4368bf533ba5ff038d3c
Cao-Tung <caotungjs@gmail.com>
2018-02-23 22:32:18 +0700

    update

--- crawler.py
+++ crawler.py
@@ -2,7 +2,6 @@ import tweepy
 import json
 import key_token
 
-
 auth = tweepy.OAuthHandler(key_token.consumer_key, key_token.consumer_secret)
 auth.set_access_token(key_token.access_token, key_token.access_token_secret)
 
@@ -12,18 +11,5 @@ stuff = api.user_timeline(screen_name = 'elonmusk', count = 100, include_rts = T
 for status in stuff:
     t = json.dumps(status._json)
     test = json.loads(t)
-    # print(type(test))
-    # print(t)
     if len(test['entities']['user_mentions']) > 0:
         print(test['entities']['user_mentions'][0]["screen_name"])
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print tweet.text
-
-# user = api.get_user('elonmusk')
-# print user.screen_name
-# print user.followers_count
-# for friend in user.friends():
-#    print friend.screen_name
-# print(user)
