1570d39ffebd3aebe3332acbcdac743db98bcad2
Cao-Tung <caotungjs@gmail.com>
2018-02-23 22:27:17 +0700

    init

--- test.py
+++ test.py
@@ -2,10 +2,7 @@ import tweepy
 import json
 from tweepy import OAuthHandler
 
-consumer_key = "luQ9n0TBdf1V8egNNcKwuk94l"
-consumer_secret = "hpENWe1p8YrgaajPDzOe0PPODIq4hWZVn9rH0mVZ8onaC0LxYz"
-access_token = "966901044822212608-ZMiHf1ZHleGZjhyZXoOzydKVoAAGAit"
-access_token_secret = "CsMJtixeYY1DkUMo12KsCsC49TxTF7wDaxFjHrGIMv4RJ"
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
